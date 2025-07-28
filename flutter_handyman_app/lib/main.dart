7bfc2904b8cc65f55a75a99e96c17f3ee138c851
Tihum-Kabir <fktihum03@gmail.com>
2025-07-28 06:58:17 +0600

    t2

--- flutter_handyman_app/lib/main.dart
+++ flutter_handyman_app/lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 //Icons need to be given picture later
 class MyHomePage extends StatelessWidget {
   static const List<Map<String, String>> services = [
